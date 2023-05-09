bb79d395ab6ae83f421721dbadd00a36dc1987ea
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-09 21:56:18 +0200

    fullpush

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+m = abs(number) % 10
+print(f'Last digit of {number} is {m} and is ', end="")
+if m > 5:
+    print('greater than 5')
+elif m == 0:
+    print(0)
+else:
+    print('less than 6 and not 0')
