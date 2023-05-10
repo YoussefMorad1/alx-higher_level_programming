42c1bdb5523f8880443ec71220cc8bdf84e54a40
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-10 03:25:12 +0200

    fullpush

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97:
-            print('{}'.format(ord(c) - 97 + ord('A')))
+        if ord(c) >= 97 and ord(c) <= ord('z'):
+            print('{}'.format(ord(c) - 97 + ord('A')), end='')
         else:
             print('{}'.format(c), end='')
