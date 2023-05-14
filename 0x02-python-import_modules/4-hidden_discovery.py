d419ea72923247296553b765b57e5600163c2f35
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-14 18:03:17 +0200

    fullpush

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import ast
-    for s in ast.parse('hidden_4'):
+    import hidden_4
+    for s in dir(hidden_4):
         if s[:2] != '__':
             print(s)
