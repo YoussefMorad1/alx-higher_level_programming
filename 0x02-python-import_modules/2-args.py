5f3e9f63de1de22875642162a06521264d0feb10
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-14 15:50:27 +0200

    fullpush

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     ln = len(argv) - 1
-    print('{} argumen{}{}'.format(ln, 't' if ln == 1 else 'ts', '.' if ln == 0 else ':'))
+    print('{} argumen{}{}'.format(ln, 't' if ln == 1 else 'ts',
+          '.' if ln == 0 else ':'))
     for i in range(1, ln + 1):
         print('{}: {}'.format(i, argv[i]))
