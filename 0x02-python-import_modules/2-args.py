ef27c22532534348ab98eaf911f0ac316d1c19eb
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-14 15:48:23 +0200

    fullpush

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ln = len(argv) - 1
+    print('{} argumen{}{}'.format(ln, 't' if ln == 1 else 'ts', '.' if ln == 0 else ':'))
+    for i in range(1, ln + 1):
+        print('{}: {}'.format(i, argv[i]))
