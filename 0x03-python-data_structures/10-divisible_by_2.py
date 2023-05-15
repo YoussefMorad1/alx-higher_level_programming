b88101afdfe79b9bdd83b3a2a35904007c04f4e0
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-15 08:45:58 +0200

    fullpush

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+def divisible_by_2(my_list=[]):
+    lst = my_list[:]
+    for i in range(len(lst)):
+        if lst[i] % 2 == 0:
+            lst[i] = True
+        else:
+            lst[i] = False
+    return lst
+
