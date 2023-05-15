545ccdef9e374380bd2462beb383d9ac36707df1
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-15 08:20:23 +0200

    fullpush

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,5 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_b) == 0):
         tuple_b = (0, 0)
     if (len(tuple_b) == 1):
-        tuple_b = (b[0], 0)
+        tuple_b = (tuple_b[0], 0)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
