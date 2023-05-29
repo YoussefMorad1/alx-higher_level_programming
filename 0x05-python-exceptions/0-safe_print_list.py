6ac6fd234a29309f341731fe89ad3f2a1871f44d
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-29 17:52:13 +0200

    fullpush

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print(my_list[i])
+            print(my_list[i], end='')
         except:
+            print()
             return i
+    print()
     return x
