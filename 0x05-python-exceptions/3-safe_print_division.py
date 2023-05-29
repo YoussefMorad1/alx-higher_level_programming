639a078ea182c8ff313e4827c73bcc56249c8d3e
Youssef Morad <youssefmohammed747@gmail.com>
2023-05-29 18:32:15 +0200

    fullpush

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         c = a / b
     except Exception:
-        c = None
+        pass
     finally:
         print("Inside result: {}".format(c))
         return c
