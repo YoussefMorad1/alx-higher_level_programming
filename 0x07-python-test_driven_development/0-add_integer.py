82376c258a36ae0d1bcbcde28344299bfa1a5c22
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-09 01:57:04 +0200

    fullpush

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     """
     This function adds to integers
     """
-    if type(a) not in (int, float):
+    if type(a) not in (int, float) or a == a + 1:
         raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
+    if type(b) not in (int, float) or b == b + 1:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
 
