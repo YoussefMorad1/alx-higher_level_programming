dbfe75d55a5e8cb8bca26cdb7a6fcb81a2750e5d
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-08 03:23:54 +0200

    fullpush

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -8,4 +8,34 @@ class Rectangle:
     """
     class represents a rectangle
     """
-    pass
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) == int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self):
+        if type(value) == int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError
+        else:
+            raise TypeError
