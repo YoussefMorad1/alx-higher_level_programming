af15f0bccc6f46d113b1d94e4de858af6b85d51c
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-08 07:51:57 +0200

    fullpush

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
             return ""
         rectangle = ""
         for i in range(self.height):
-            rectangle += str(Rectangle.print_symbol) * self.width + \
+            rectangle += str(self.print_symbol) * self.width + \
                 ("\n" if i < self.height - 1 else "")
         return rectangle
 
