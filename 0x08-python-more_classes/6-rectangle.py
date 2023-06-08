db5fd618bab8eb34516bcd099de37b4d1cc7ff47
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-08 04:53:08 +0200

    fullpush

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,9 +8,11 @@ class Rectangle:
     """
     class represents a rectangle
     """
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        number_of_instances += 1
 
     @property
     def width(self):
