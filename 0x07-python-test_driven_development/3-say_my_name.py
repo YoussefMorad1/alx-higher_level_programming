e67e1a6ff105a2ca95fb0b131975c600003b565d
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-09 04:38:28 +0200

    fullpush

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module says 'say my name'
+"""
+
+
+def say_my_name(fname, lname):
+    """
+    say my name function
+    """
+    if type(fname) != str:
+        raise TypeError('first_name must be a string')
+    if type(lname) != str:
+        raise TypeError('last_name must be a string')
+    print(f'My name is {fname} {lname}')
