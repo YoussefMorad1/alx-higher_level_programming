8080ce94e616aa9489fb82d0d4e8d78c511f7c41
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 23:07:34 +0200

    fullpush

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        """ constructor method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, att=None):
+        """ to json method """
+        dic = dict(vars(self))
+        keys = list(dic.keys())
+        for key in keys:
+            if att is not None and key not in att:
+                del dic[key]
+        return dic
+
+    def reload_from_json(self, json):
+        """ reload from json """ 
+        for key in json.keys():
+            self[key] = json[key]
