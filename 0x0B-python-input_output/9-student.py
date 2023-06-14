5e0cb1d132cdaee2b67295138def35b363060c55
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 22:44:49 +0200

    fullpush

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,8 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """ to json method """
-        return {'first_name':self.first_name, 'last_name':self.last_name, 'age':self.age}
+        return {'first_name': self.first_name,
+                'last_name': self.last_name, 'age': self.age}
