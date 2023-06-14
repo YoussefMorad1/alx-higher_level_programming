3529849c5b228acf8b562c494a11ec5b07b75ba7
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 22:52:16 +0200

    fullpush

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,6 +10,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, att=None):
         """ to json method """
-        return vars(self)
+        dic = vars(self)
+        for key in dic.keys():
+            if att is not None and key not in att:
+                del dic[key]
+        return dic
