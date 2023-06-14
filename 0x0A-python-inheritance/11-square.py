488befc7504946228d303ecc8dee18ca9fd7f630
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 10:22:28 +0200

    fullpush

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module of square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Class """
+    def __init__(self, size):
+        """
+        constructor of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ calc area of square """
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
