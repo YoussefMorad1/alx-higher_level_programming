825915c356e3c4e2fe1b9bb0473f9c73d1f3e00a
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 23:12:05 +0200

    fullpush

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -22,4 +22,4 @@ class Student:
     def reload_from_json(self, json):
         """ reload from json """ 
         for key in json.keys():
-            self[key] = json[key]
+            setattr(self, key, json[key])
