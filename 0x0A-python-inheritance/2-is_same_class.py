3b78e70a01019db5542220064fd2bb1c913951ad
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-14 09:04:49 +0200

    fullpush

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module new """
+
+
+def is_same_class(obj, a_class):
+    """ Checks if the object is exactly an instance of the specified class """
+    return type(obj) == a_class
