250db7d192fbb2c21e901a0205b7cbc94e5acadf
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-20 04:02:01 +0200

    fullpush

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from base import Base
 
 
 class Rectangle(Base):
-    """Rectangle Class"""
+    """Rectangle Class that really loves you"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor"""
