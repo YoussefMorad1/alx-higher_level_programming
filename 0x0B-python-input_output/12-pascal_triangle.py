de29f07c8145183b803337a1a306b30b5474dc29
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-20 20:33:40 +0200

    fullpush

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" module important much """
-
-
+"""
+pascal traingle module
+"""
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    l = [[0 for x in range(n)] for y in range(n)]
-
+    """
+    pascal function returns list of list
+    """
+    res = []
+    for i in range(n):
+        res.append([0 for _ in range(i+1)])
+    res[0][0] = 1
+    for i in range(n-1):
+        for j in range(i+1):
+            res[i+1][j] += res[i][j]
+            if j+1 < n:
+                res[i+1][j+1] += res[i][j]
+    return res
